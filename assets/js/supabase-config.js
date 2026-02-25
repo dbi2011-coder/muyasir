a542c394d2b2b86e6fef3f1e0c94c61208edd758
dbi2011-coder <dbi2011@hotmail.com>
2026-02-25 14:50:15 +0300

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -2,9 +2,8 @@
 // 📁 الملف: assets/js/supabase-config.js
 // ============================================
 
-// استخراج الرابط من التوكن الخاص بك
 const SUPABASE_URL = 'https://xcygxsfkwuuxaoqpsoso.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjeWd4c2Zrd3V1eGFvcXBzb3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMTI0NzMsImV4cCI6MjA4NzU4ODQ3M30.kvEEey7hxX7F4A8W-x6d4zDfXDkCepj2Z9lmifBJ4x4';
 
-// تهيئة مكتبة Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// أسميناه supa بدلاً من supabase لتجنب التعارض
+const supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
